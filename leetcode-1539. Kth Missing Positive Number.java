e4fae9f1eea4d34f1ee9ba177975a1687f437b4e
shivarv <shivarv1993@gmail.com>
2026-03-07 19:50:48 +0530

    leetcode 1539

--- leetcode-1539. Kth Missing Positive Number.java
+++ leetcode-1539. Kth Missing Positive Number.java
@@ -22,27 +22,28 @@ class Solution {
 
         return arr[arr.length - 1] + k;
     }
-    /*
-    public int findKthPositive(int[] arr, int k) {
-        int missingNums = 0;
-        if(arr[0] > 1) {
-            missingNums = arr[0] - 1;
-        }
-        if(k <= missingNums) {
-            return k;
-        } else {
-            k -= missingNums;
-        }
-        for(int i = 1; i < arr.length; i++) {
-            System.out.println(i  + " k " +k);
-            if(arr[i] - arr[i -1] != 1) {
-                if(arr[i] - arr[i -1] >= k + 1) {
-                    return arr[i - 1] + k;
-                }
-                k = k - (arr[i] - arr[i -1]) + 1;
-            }
-           
-        }
-        return arr[arr.length - 1] + k;
-    } */
+	
 }
+//best solution
+/*
+
+class Solution {
+    //my understanding of this algo is
+    // arr[mid] - (mid +1) is supposed to be 0, anything else means 
+    //we need to move left or right
+    // the left essentially sits at the place where + k gives us the answer
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
+*/
